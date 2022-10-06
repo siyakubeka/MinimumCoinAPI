f885995ff4ddc54450c4ec19641f0404b2a9170a
skubeka <skubeka@SKUBEKA>
2022-10-06 08:36:40 +0200

    Add project files.

---
+++ MinimumCoinAPI/Controllers/CalculationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinimumCoinAPI.models;
+namespace MinimumCoinAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculationController : ControllerBase
+    {
+        
+        Calculation calculation = new Calculation();
+        [HttpGet]
+        public int Get()
+        {
+            //Get the Different denominations of coins
+            int[] coins = { 25, 10, 5 };
+
+            //Get the number of coins
+            int m = coins.Length;
+
+            //The amount that need to be paid out
+            int Y = 30;
+
+            //Send the information above to the Calculation model and return the minimum number of coins needed to make change for a given amount of Y
+            return calculation.countMinCoin(coins, m, Y);
+        }
+
+    }
+}
---
+++ MinimumCoinAPI/models/Calculation.cs
@@ -0,0 +1,33 @@
+﻿namespace MinimumCoinAPI.models
+{
+    public class Calculation
+    {
+        //Here we use a recursive structure   
+        public int countMinCoin(int[] coin, int m, int Y)
+        {
+            //Here lies the base case, if Y == 0, then 0 coins returned
+            if (Y == 0)
+                return 0;
+
+            int minCount = int.MaxValue;
+            // Here Brute force algorithm with recursion
+            for (int i = 0; i < m; i = i + 1)
+            {
+                if (coin[i] <= Y)
+                {
+                    int currCount = countMinCoin(coin, m, Y - coin[i]);
+                    if (currCount != int.MaxValue && currCount + 1 < minCount)
+                        minCount = currCount + 1;
+                }
+
+            }
+            if (minCount == int.MaxValue)
+                return -1;
+            else
+                return minCount;
+        }
+        
+
+
+    }
+}
